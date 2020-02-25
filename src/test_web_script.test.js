d70b23bbb6d78a6d1ca0d3843a3e8dbe03d3f2c7
wearypossum4770 <wearypossum4770@yahoo.com>
2020-02-24 22:34:26 -0600

    Revert "switching to mac-mini"
    This reverts commit dae02f470fa1d1c88a458dd424c3f5c9143f1b71.

--- src/test_web_script.test.js
+++ src/test_web_script.test.js
@@ -1,8 +1,1 @@
-const ghpages = require('gh-pages');
-ghpages.publish('docs', function(err) {
-add:true,
-branch: 'gh-pages',
-mssage: 'Auto-generated commit gh-pages module',
-remote:'upstream',
-user: {name: 'wearypossum4770', email: 'wearypossum4770@yahoo.com'}
-});
+
